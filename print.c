8886811165099ba05554256fd02f52ab76c08d9e
jerry.li <18016020997@163.com>
2019-01-30 20:08:14 +0800

    new version

---
+++ print.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "print.h"
+
+unsigned char print_picture(unsigned char number)
+{
+	switch(number){
+		case 1:
+		break;
+		case 2:
+		break;
+		default:
+		break;
+	}
+}
